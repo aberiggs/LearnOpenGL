809ab9e8dc589cb81b26d02b533258922efc1917
aberiggs <sprocket710@gmail.com>
2024-02-24 01:43:08 -0500

    🔺

--- getting-started/src/main.cpp
+++ getting-started/src/main.cpp
@@ -1,7 +1,34 @@
+/* Learning OpenGL by drawing a triangle */
+
 #include <glad/glad.h>
 #include <iostream>
 
 #include <GLFW/glfw3.h>
+
+// Vertex data format: <x,y,z>
+float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
+unsigned int VBO{};
+unsigned int vertexShader{};
+unsigned int fragmentShader{};
+unsigned int shaderProgram{};
+unsigned int VAO{};
+
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    " FragColor = vec4(1.0f, 0.36f, 0.69f, 1.0);\n"
+    "}\0";
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -11,6 +38,45 @@ void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
 }
+
+// NOTE: Call before creating fragment shader
+void createVertexShader() {
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Vertex attribute linking
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Actual vetex shader
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+}
+
+// NOTE: Call before creating shader program
+void createFragmentShader() {
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+}
+
+void createShaderProgram() {
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+}
+
+void generateVAO() {}
 
 int main() {
   glfwInit();
@@ -36,14 +102,23 @@ int main() {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  createVertexShader();
+  createFragmentShader();
+  createShaderProgram();
+
   // Render loop
   while (!glfwWindowShouldClose(window)) {
     // Input
     processInput(window);
 
-    // Render commands here
+    // Draw background
     glClearColor(0.16f, 0.16f, 0.16f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw object
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Check, call events, and swap buffers
     glfwSwapBuffers(window);
