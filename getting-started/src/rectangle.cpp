f8bb6a43d6e071faea6c5154621f88b7662263f9
aberiggs <sprocket710@gmail.com>
2024-02-25 01:26:24 -0500

    🔺 + 🟥

---
+++ getting-started/src/rectangle.cpp
@@ -0,0 +1,146 @@
+/* Learning OpenGL by drawing a triangle */
+
+#include <glad/glad.h>
+#include <iostream>
+
+#include <GLFW/glfw3.h>
+
+// Vertex data format: <x,y,z>
+float vertices[] = {
+    // First triangle
+     0.5f,  0.5f, 0.0f, // Top right
+     0.5f, -0.5f, 0.0f, // Bottom right
+    -0.5f, -0.5f, 0.0f, // Bottom left
+    -0.5f,  0.5f, 0.0f // Top left
+};
+
+unsigned int indices[] = {
+    0, 1, 3,  // First triangle 
+    1, 2, 3   // Second triangle
+};
+
+unsigned int VBO{};
+unsigned int vertexShader{};
+unsigned int fragmentShader{};
+unsigned int shaderProgram{};
+unsigned int VAO{};
+unsigned int EBO{};
+
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    " FragColor = vec4(1.0f, 0.36f, 0.69f, 1.0);\n"
+    "}\0";
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+// NOTE: Call before creating fragment shader
+void createVertexShader() {
+  glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // Vertex attribute linking
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  // Actual vetex shader
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+}
+
+// NOTE: Call before creating shader program
+void createFragmentShader() {
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+}
+
+void createShaderProgram() {
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+}
+
+void generateVAO() {}
+
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD\n";
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  createVertexShader();
+  createFragmentShader();
+  createShaderProgram();
+
+  // Render loop
+  while (!glfwWindowShouldClose(window)) {
+    // Input
+    processInput(window);
+
+    // Draw background
+    glClearColor(0.16f, 0.16f, 0.16f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Draw object
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO); 
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    // Check, call events, and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
